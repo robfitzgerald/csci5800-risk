6db2532ce588b20a63d694bbbdb02ec61e5510fe
Robert Fitzgerald <robfitzgerald@gmail.com>
2016-03-20 16:11:14 -0600

    lol. re-normalized; aka, changed around some comments and where things are placed.

--- mcts-game-server/lib/backup.js
+++ mcts-game-server/lib/backup.js
@@ -1,9 +1,8 @@
 'use strict';
 {
 	module.exports = function() {
+		var knowledgeBase = require('./knowledgeBase')
 		// a neo4j call to update the reward and visit count
 		// and propogate that to all parents.
 	}
-
-	var knowledgeBase = require('./knowledgeBase')
 }
--- mcts-game-server/lib/treePolicy.js
+++ mcts-game-server/lib/treePolicy.js
@@ -1,12 +1,9 @@
 'use strict';
 {
 	module.exports = function() {
+		var knowledgeBase = require('knowledgeBase');
 
+		// construct the query/ies to call neo4j and select the next node
+		// via the multi armed bandit selection process
 	}
-		
-	var knowledgeBase = require('knowledgeBase');
-
-	// construct the query/ies to call neo4j and select the next node
-	// via the multi armed bandit selection process
-
 }

21228c81ca650cadc2de86acda6c0d209425fe7a
W. Max Lees <max.lees@gmail.com>
2016-03-25 15:19:26 -0600

    Added basic GET for simulation

--- mcts-sim-server/main.go
+++ mcts-sim-server/main.go
@@ -1,10 +1,37 @@
 package main
 
-import "github.com/wmaxlees/clips"
-import "fmt"
+import ("github.com/wmaxlees/clips"
+        "fmt"
+        "github.com/emicklei/go-restful"
+        "net/http"
+        "strings")
 // import "github.com/wmaxlees/clips/factparser"
 
 func main () {
+    ws := new(restful.WebService)
+
+    ws.Path("/sim").
+        Consumes(restful.MIME_JSON).
+        Produces(restful.MIME_JSON)
+
+    ws.Route(ws.GET("/{state}").To(runSimulation))
+
+    restful.Add(ws)
+    http.ListenAndServe(":8080", nil)
+
+    //list := clips.GetFactList()
+    //fmt.Printf(list)
+
+    // act := clips.GetNextActivation()
+    // str := clips.GetActivationName(act)
+    // fmt.Printf(str)
+}
+
+func runSimulation(request *restful.Request, response *restful.Response) {
+    state := request.PathParameter("state")
+
+    fmt.Printf("%s\n", state)
+
     clips.CreateEnvironment()
 
     clips.Load("risk.clips")
@@ -16,12 +43,6 @@ func main () {
     clips.Run(-1)
 
     resultPtr := clips.FindGlobal("result")
-    fmt.Printf("%s\n", clips.GetGlobalValueForm(resultPtr, 15))
 
-    //list := clips.GetFactList()
-    //fmt.Printf(list)
-
-    // act := clips.GetNextActivation()
-    // str := clips.GetActivationName(act)
-    // fmt.Printf(str)
+    response.WriteAsJson("{\"result\": " + strings.Split(clips.GetGlobalValueForm(resultPtr, 15), " ")[2] + "}")
 }

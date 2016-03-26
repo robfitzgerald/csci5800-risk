36c934541daa38284a9e98915b360a739d325a0e
W. Max Lees <max.lees@gmail.com>
2016-03-25 21:02:32 -0600

    Simulation endpoint created

--- mcts-sim-server/main.go
+++ mcts-sim-server/main.go
@@ -4,45 +4,108 @@ import ("github.com/wmaxlees/clips"
         "fmt"
         "github.com/emicklei/go-restful"
         "net/http"
-        "strings")
+        "strings"
+        "strconv")
 // import "github.com/wmaxlees/clips/factparser"
+
+type Country struct {
+    Name string
+    Player int
+    Armies int
+}
+
+type State struct {
+    Players int
+    Free int
+    Phase string
+    Countries []Country
+}
 
 func main () {
     ws := new(restful.WebService)
 
-    ws.Path("/sim").
+    ws.Path("/").
         Consumes(restful.MIME_JSON).
         Produces(restful.MIME_JSON)
 
-    ws.Route(ws.GET("/{state}").To(runSimulation))
+    ws.Route(ws.POST("sim").To(runSimulation))
+    ws.Route(ws.GET("actions/{state}").To(getActions))
 
     restful.Add(ws)
     http.ListenAndServe(":8080", nil)
-
-    //list := clips.GetFactList()
-    //fmt.Printf(list)
-
-    // act := clips.GetNextActivation()
-    // str := clips.GetActivationName(act)
-    // fmt.Printf(str)
 }
 
-func runSimulation(request *restful.Request, response *restful.Response) {
+
+func getActions(request *restful.Request, response *restful.Response) {
     state := request.PathParameter("state")
+    fmt.Printf("Actions: %s\n", state)
 
-    fmt.Printf("%s\n", state)
+    // assertState(state)
 
-    clips.CreateEnvironment()
+    env := clips.CreateEnvironment()
+    clips.Load(env, "risk.clips")
 
-    clips.Load("risk.clips")
+    clips.Reset(env)
 
-    clips.SetStrategy(clips.RANDOM_STRATEGY)
+    // Assert the state
+    // Get the possible actions
 
-    clips.Reset()
 
-    clips.Run(-1)
+    clips.Clear(env)
 
-    resultPtr := clips.FindGlobal("result")
+    response.WriteAsJson("STUB")
+}
 
-    response.WriteAsJson("{\"result\": " + strings.Split(clips.GetGlobalValueForm(resultPtr, 15), " ")[2] + "}")
+
+func runSimulation(request *restful.Request, response *restful.Response) {
+    // state := request.PathParameter("state")
+
+    state := new(State)
+    request.ReadEntity(state)
+
+    fmt.Printf("Sim: %s\n", state)
+
+    env := clips.CreateEnvironment()
+    clips.Load(env, "risk.clips")
+    clips.Reset(env)
+    assertState(env, *state)
+    clips.SetStrategy(env, clips.RANDOM_STRATEGY)
+
+    clips.Run(env, -1)
+
+    resultPtr := clips.FindGlobal(env, "result")
+
+    response.WriteAsJson("{\"result\": " + strings.Split(clips.GetGlobalValueForm(env, resultPtr, 15), " ")[2] + "}")
+}
+
+func assertState(env clips.EnvironmentPointer, state State) {
+    assertPlayers(env, state.Players)
+    assertTurn(env, 0)
+    assertPhase(env, state.Phase)
+    assertFreeArmies(env, state.Free)
+
+    for _, country := range state.Countries {
+        assertCountry(env, country.Name, country.Player, country.Armies)
+    }
+}
+
+func assertPlayers(env clips.EnvironmentPointer, num int) {
+    clips.AssertString(env, "(players " + strconv.Itoa(num) + ")")
+}
+
+func assertTurn(env clips.EnvironmentPointer, num int) {
+    clips.AssertString(env, "(turn " + strconv.Itoa(num) + ")")
+}
+
+func assertPhase(env clips.EnvironmentPointer, phase string) {
+    clips.AssertString(env, "(phase (current " + phase + "))")
+}
+
+func assertFreeArmies(env clips.EnvironmentPointer, num int) {
+    clips.AssertString(env, "(free-armies " + strconv.Itoa(num) + ")")
+}
+
+func assertCountry(env clips.EnvironmentPointer, name string, player int, armies int) {
+    clips.AssertString(env, "(controls (country " + name + ") (player " + strconv.Itoa(player) + "))")
+    clips.AssertString(env, "(armies (country " + name + ") (num " + strconv.Itoa(armies) + "))")
 }

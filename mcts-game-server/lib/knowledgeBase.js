d15f8bfcee70ebf6b367f6bd97eadfe6d4084f82
Robert Fitzgerald <robfitzgerald@gmail.com>
2016-03-26 22:18:41 -0600

    set all requirements to send requests to neo4j, including new config, authentication header, and a starting create method which creates a solo (root) node.

--- mcts-game-server/lib/knowledgeBase.js
+++ mcts-game-server/lib/knowledgeBase.js
@@ -3,19 +3,60 @@
 'use strict';
 {
 	module.exports = {
-		update,
-		parentOf,
-		childOf,
-		relatedAction
+		createNewRoot
 	}
-	var request = require('request');
 
-	function update(variant, board, value) {}
+	var config = require('config')
+		, auth = new Buffer(config.get('neo4j.username') + ':' + config.get('neo4j.password'))
+		, request = require('request')
+		, neo4j = request.defaults({
+			method: 'POST',
+			url: config.get('neo4j.baseUrl'),
+			headers: {
+				Authorization: 'Basic ' + auth.toString('base64')
+			}
+		})
 
-	function parentOf(variant, board) {}
+	function createNewRoot(b, moves, variant) {
+		var board = '--BOARDTEST--p1:human,;p2:ai,'//variant.serialize(b)  // if b is JSON
+			, index = hashBoard(board)
+			, params = {
+				boardParams: {
+					expanded: 'true',
+					state: board,
+					expandableMoves: moves,
+					rewards: [],
+					visits: 0,
+					usedInGame: []
+				}
+			}
+			, query = 'CREATE (p:BOARD {boardParams}) RETURN p'
+			console.log('query')
+			console.log(query);
 
-	function childOf(variant, board) {}
+			neo4j(
+				{json:
+					{statements: [
+						{
+							statement: query,
+							parameters: params
+						}
+					]
+				}
+			}, function(err, res, body) {
+				console.log(body)
+			});
+	}
 
-	function relatedAction(variant, parent, child) {}
+	function isNonTerminal(b) {
+
+	}
+
+	function hashBoard(board) {
+		var output = new Buffer(board)
+		return output.toString('base64');
+	}
 	
+	//createNewRoot(null, ['move1', 'move2'], null);
+	//isNonTerminal(b)
 }

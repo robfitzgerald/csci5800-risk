e7ea560ce25d0c088351324d6972c764e9aa0681
Robert Fitzgerald <robfitzgerald@gmail.com>
2016-03-26 22:50:14 -0600

    added another neo4j call, a boolean MATCH function. added helper functions.

--- mcts-game-server/lib/knowledgeBase.js
+++ mcts-game-server/lib/knowledgeBase.js
@@ -3,7 +3,8 @@
 'use strict';
 {
 	module.exports = {
-		createNewRoot
+		createNewRoot,
+		createChild
 	}
 
 	var config = require('config')
@@ -17,46 +18,92 @@
 			}
 		})
 
+	/**
+	 * creates a root node in the knowledge base
+	 * @param  {Board} b        - game board object
+	 * @param  {Object[]} moves - list of valid moves
+	 * @param  {Object} variant - game variant object used to serialize/deserialize board states
+	 * @return {Promise}        - Promise that returns created node on success
+	 */
 	function createNewRoot(b, moves, variant) {
 		var board = '--BOARDTEST--p1:human,;p2:ai,'//variant.serialize(b)  // if b is JSON
 			, index = hashBoard(board)
 			, params = {
 				boardParams: {
-					expanded: 'true',
-					state: board,
-					expandableMoves: moves,
+					nonTerminal: 'true',
+					state: index,
+					possibleMoves: moves,
 					rewards: [],
 					visits: 0,
 					usedInGame: []
 				}
 			}
-			, query = 'CREATE (p:BOARD {boardParams}) RETURN p'
-			console.log('query')
-			console.log(query);
+			, statement = 'CREATE (p:BOARD {boardParams}) RETURN p'
+			, payload = constructQueryBody([statement], [params])
 
-			neo4j(
-				{json:
-					{statements: [
-						{
-							statement: query,
-							parameters: params
-						}
-					]
-				}
-			}, function(err, res, body) {
+			neo4j({json:payload}, function(err, res, body) {
 				console.log(body)
+				isNonTerminal(board) // just in here for testing
+				// TODO: deferred.resolve() / deferred.reject() of promise
 			});
 	}
 
-	function isNonTerminal(b) {
 
+	function createChild(p, move, c) {
+
+	}
+
+	/**
+	 * checks if a given board is a non-terminal node
+	 * @param  {Board}  b  - board state
+	 * @return {Promise}   - promise that resolves to a boolean
+	 */
+	function isNonTerminal(b) {
+		var index = hashBoard(b)
+			, query = `MATCH (b:BOARD{state:'${index}'}) RETURN b.nonTerminal`
+			, payload = constructQueryBody(query);
+		neo4j({json:payload}, function(err, response, body) {
+			console.log('isNonTerminal() result:')
+			console.log(JSON.stringify(body))
+			console.log('resolve promise w/ boolean')
+		})
+	}
+
+	/**
+	 * checks if a given board is fully expanded
+	 * @param  {Board}  b  - board state
+	 * @return {Promise}   - promise that resolves to a boolean
+	 */
+	function isFullyExpanded(b) {
+		var index = hashBoard(b)
+			, query = `MATCH (b:BOARD{state:${index}}) RETURN b.possibleMoves.length`
+			, payload = constructQueryBody(query)
+		neo4j({json:payload}, function(err, response, body) {
+			console.log('resolve promise')
+			// TODO: find out how to check length of array
+		})
 	}
 
 	function hashBoard(board) {
 		var output = new Buffer(board)
 		return output.toString('base64');
 	}
-	
-	//createNewRoot(null, ['move1', 'move2'], null);
-	//isNonTerminal(b)
+
+	function constructQueryBody(statements, parameters) {
+		if (!Array.isArray(statements) || !Array.isArray(parameters || statements.length != parameters.length)) {
+			if (statements) {
+				return {statements:[{statement:statements}]};
+			} else {
+				return {statements: []};
+			}
+		} else {
+			var output = {statements:[]}
+			for (var i = 0; i < statements.length; ++i) {
+				output.statements.push({statement: statements[i], parameters: parameters[i]});
+			}
+			return output;
+		}
+	}
+
+	createNewRoot(null, ['move1', 'move2'], null);
 }

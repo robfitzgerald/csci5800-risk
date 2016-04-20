1bb61b585ab1106d0d3c703a17a8fd509f387635
W. Max Lees <max.lees@gmail.com>
2016-04-20 12:57:34 -0600

    basic expand working

--- mcts-game-server/gameResources/expand.js
+++ mcts-game-server/gameResources/expand.js
@@ -12,6 +12,86 @@
         , clips = require('clips-module')
         , Q = require('q')
 
+    function attackhalf (generalizedBoard, action) {
+        var deferred = Q.defer();
+
+        var aRolls;
+        var aArmies = Math.round(generalizedBoard.Countries[action.params[0]].Armies);
+        if (aArmies > 3) {
+            aRolls = 3;
+        } else if (aArmies > 2) {
+            aRolls = 2;
+        } else {
+            aRolls = 1;
+        }
+
+        var dRolls;
+        var dArmies = generalizedBoard.Countries[action.params[1]].Armies;
+        if (dArmies > 1) {
+            dRolls = 2;
+        } else {
+            dRolls = 1;
+        }
+
+        var rolls = Math.max(aRolls, dRolls);
+        var results = [];
+        var arr = [];
+        for (int i = 0; i < rolls; ++i) {
+            arr.push(i);
+        }
+
+        async.each(arr, function(wins, callback) {
+            var temp = applyAction.attackhalf(generalizedBoard, action.params, wins, rolls-wins);
+            clips.generateChildren(temp).then(function (value) {
+                result.push(value);
+            });
+        }, function (err) {
+            deferred.resolve(arr);
+        });
+
+        return deferred.promise;
+    }
+
+    function attackall (generalizedBoard, action) {
+        var deferred = Q.defer();
+
+        var aRolls;
+        var aArmies = generalizedBoard.Countries[action.params[0]].Armies;
+        if (aArmies > 3) {
+            aRolls = 3;
+        } else if (aArmies > 2) {
+            aRolls = 2;
+        } else {
+            aRolls = 1;
+        }
+
+        var dRolls;
+        var dArmies = generalizedBoard.Countries[action.params[1]].Armies;
+        if (dArmies > 1) {
+            dRolls = 2;
+        } else {
+            dRolls = 1;
+        }
+
+        var rolls = Math.max(aRolls, dRolls);
+        var results = [];
+        var arr = [];
+        for (int i = 0; i < rolls; ++i) {
+            arr.push(i);
+        }
+
+        async.each(arr, function(wins, callback) {
+            var temp = applyAction.attackall(generalizedBoard, action.params, wins, rolls-wins);
+            clips.generateChildren(temp).then(function (value) {
+                result.push(value);
+            });
+        }, function (err) {
+            deferred.resolve(arr);
+        });
+
+        return deferred.promise;
+    }
+
     function fortifymove (generalizedBoard, action) {
         var deferred = Q.defer();
 
